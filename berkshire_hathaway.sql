166aeb5c5bf399938458f38a11dd442c3fe24a09
Vatsal Gayakwad <vatsalgayakwad@Vatsals-MacBook-Air.local>
2023-08-19 17:09:50 +0530

    First Commit

---
+++ berkshire_hathaway.sql
@@ -0,0 +1,17 @@
+CREATE TABLE berkshire_hathaway (
+    number INTEGER,
+    stock_symbol TEXT,
+    company_name TEXT,
+    percentage_portfolio REAL,
+    shares_owned INTEGER,
+    value_of_shares_owned INTEGER,
+    current_price FLOAT,
+    divident_yield FLOAT,
+    annual_dividents FLOAT
+);
+
+
+.mode csv
+.import /Users/vatsalgayakwad/downloads/Berkshire_Hathaway_Holdings.csv berkshire_hathaway
+
+SELECT * FROM berkshire_hathaway;
